1711d51763b357e23b82ef93723cd40f042e487d
mayankpatel1004 <mayank.patel104@gmail.com>
2021-09-30 17:44:52 +0530

    Demonstration Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {useSelector, useDispatch} from "react-redux";
+import {incNumber, decNumber} from "./actions/index";
 
 function App() {
+
+  const myState = useSelector((state) => state.changeTheNumber);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      Hello World
+      <a onClick={() => dispatch(decNumber())}>Decrement</a>
+      <input type="text" name="quantity" value={myState} />
+      <a onClick={() => dispatch(incNumber())}>Increment</a>
     </div>
   );
 }
-
 export default App;
